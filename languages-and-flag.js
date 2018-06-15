6ee639a504d5d687d11b1714ef25b4b08736cae3
Irevall <kubajmarek@gmail.com>
2018-06-15 14:34:48 +0200

    Fix initial lag

--- languages-and-flag.js
+++ languages-and-flag.js
@@ -22,49 +22,50 @@ let i18n = new VueI18n({
     messages,
 });
 
-if (localStorage.getItem('lang') === null) {
-    localStorage.setItem('lang', 'en');
-    i18n.locale ='en';
-} else {
-    i18n.locale = localStorage.getItem('lang');
-    flagSwitch(i18n.locale);
-}
-
-
-new Vue({i18n}).$mount('nav');
-
-document.querySelector('.flags').querySelectorAll('img').forEach((element) => {
-    element.addEventListener('click', () => {
-        if (i18n.locale === 'en') {
-            localStorage.setItem('lang', 'pl');
-            i18n.locale = 'pl';
-            flagSwitch('pl');
-        } else {
-            localStorage.setItem('lang', 'en');
-            i18n.locale = 'en';
-            flagSwitch('en');
-        }
-    });
-    element.addEventListener('mouseover', (e) => {
-       if (i18n.locale === 'pl') {
-           flagSwitch('en')
-       } else {
-           flagSwitch('pl');
-       }
-    });
-
-    element.addEventListener('mouseout', (e) => {
-        flagSwitch(i18n.locale);
-    });
-});
-
-function flagSwitch(code) {
-    const flags = document.querySelector('.flags').querySelectorAll('img');
-    if (code === 'pl') {
-        flags[0].classList.add('hidden');
-        flags[1].classList.remove('hidden');
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('lang') === null) {
+        localStorage.setItem('lang', 'en');
+        i18n.locale ='en';
     } else {
-        flags[0].classList.remove('hidden');
-        flags[1].classList.add('hidden');
+        i18n.locale = localStorage.getItem('lang');
+        flagSwitch(i18n.locale);
     }
-}
+
+    new Vue({i18n}).$mount('nav');
+
+    document.querySelector('.flags').querySelectorAll('img').forEach((element) => {
+        element.addEventListener('click', () => {
+            if (i18n.locale === 'en') {
+                localStorage.setItem('lang', 'pl');
+                i18n.locale = 'pl';
+                flagSwitch('pl');
+            } else {
+                localStorage.setItem('lang', 'en');
+                i18n.locale = 'en';
+                flagSwitch('en');
+            }
+        });
+        element.addEventListener('mouseover', (e) => {
+           if (i18n.locale === 'pl') {
+               flagSwitch('en')
+           } else {
+               flagSwitch('pl');
+           }
+        });
+
+        element.addEventListener('mouseout', (e) => {
+            flagSwitch(i18n.locale);
+        });
+    });
+
+    function flagSwitch(code) {
+        const flags = document.querySelector('.flags').querySelectorAll('img');
+        if (code === 'pl') {
+            flags[0].classList.add('hidden');
+            flags[1].classList.remove('hidden');
+        } else {
+            flags[0].classList.remove('hidden');
+            flags[1].classList.add('hidden');
+        }
+    }
+});
