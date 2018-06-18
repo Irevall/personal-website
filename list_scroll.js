0733a86e5076de6804f377b0066a5b430152f1a4
Irevall <kubajmarek@gmail.com>
2018-06-18 04:54:51 +0200

    Add scripts to main, add polish resume

--- list_scroll.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.title').querySelectorAll('li').forEach((element, index) => {
-        element.addEventListener('click', () => {
-            document.querySelector('main').querySelectorAll('article')[index].scrollIntoView({block: "start", behavior: "smooth"});
-        });
-    });
-});
--- main.js
+++ main.js
@@ -22,7 +22,37 @@ let i18n = new VueI18n({
     messages,
 });
 
+function polygonResize() {
+    const width =  window.innerWidth || document.body.clientWidth;
+    const svg = document.querySelector('footer').querySelectorAll('svg');
+    const polygons = document.querySelector('footer').querySelectorAll('polygon');
+    const height = 30 + width / 30;
+    document.querySelector('footer').style.height = height + 'px';
+    svg[0].style.height = height + 'px';
+    svg[1].style.height = height + 'px';
+    polygons[0].points[1].x = width;
+    polygons[0].points[2].x = width;
+    polygons[1].points[2].x = width;
+    polygons[0].points[0].y = height;
+    polygons[0].points[2].y = height;
+    polygons[1].points[1].y = height;
+    polygons[1].points[2].y = height;
+}
+
+function flagSwitch(code) {
+    const flags = document.querySelector('.flags').querySelectorAll('img');
+    if (code === 'pl') {
+        flags[0].classList.add('hidden');
+        flags[1].classList.remove('hidden');
+    } else {
+        flags[0].classList.remove('hidden');
+        flags[1].classList.add('hidden');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    polygonResize();
+
     if (localStorage.getItem('lang') === null) {
         localStorage.setItem('lang', 'en');
         i18n.locale ='en';
@@ -33,6 +63,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     new Vue({i18n}).$mount('nav');
+
+    window.addEventListener('resize', () => {
+        polygonResize();
+    });
 
     document.querySelector('.flags').querySelectorAll('img').forEach((element) => {
         element.addEventListener('click', () => {
@@ -45,7 +79,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 i18n.locale = 'en';
                 flagSwitch('en');
             }
+            if (window.location.pathname==='/resume') {
+                document.querySelector('embed').src = 'misc/resume-' + i18n.locale + '.pdf';
+            }
         });
+
         element.addEventListener('mouseover', (e) => {
             if (document.body.clientWidth >= 600) {
                 if (i18n.locale === 'pl') {
@@ -69,16 +107,6 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('nav').classList.remove('extended');
     });
 
-    function flagSwitch(code) {
-        const flags = document.querySelector('.flags').querySelectorAll('img');
-        if (code === 'pl') {
-            flags[0].classList.add('hidden');
-            flags[1].classList.remove('hidden');
-        } else {
-            flags[0].classList.remove('hidden');
-            flags[1].classList.add('hidden');
-        }
-    }
     // console.log(window.innerHeight);
     // console.log(document.querySelector('main').clientHeight + document.querySelector('nav').clientHeight + document.querySelector('footer').clientHeight);
     // console.log(document.body.clientHeight);
